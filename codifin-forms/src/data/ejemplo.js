db09aab1a5f55b66a0fba1adda0ed534ce779077
ASA-17 <123484170+ASA-17@users.noreply.github.com>
2023-02-20 21:59:31 -0600

    Update ejemplo.js

--- codifin-forms/src/data/ejemplo.js
+++ codifin-forms/src/data/ejemplo.js
@@ -1,14 +1,82 @@
 export const ejemplo = [
     {
         id: 1,
-        question: "¿Cual es tu idioma favorito?",
-        answers: ["Español", "Ingles", "Frances"]
-
+        question: "¿Cuál es el perfil tech que se busca cubrir?",
+        answers: ["Front-End", "Back-End", "Full Stack", "Data Science & AI", "Cloud", "Android", "iOS", "Diseño UI/UX", "Base de Datos", "Software Architect", "Otro"]
     },
     {
         id: 2,
-        question: "¿Cual es tu comida favorita?",
-        answers: ["Pizza", "Tacos", "Pan"]
-
+        question: "Descripción breve de la vacante",
+        answers: [" "]
+    },
+    {
+        id: 3,
+        question: "¿Qué nivel de experiencia debe tener el candidato ideal?",
+        answers: ["JR (6 meses + experiencia en el rol)", "Semi Sr (2 años + experiencia en el rol)", "Sr (4 años + experiencia en el rol)"]
+    },
+    {
+        id: 4,
+        question: "¿Dentro del perfil tech que lenguajes debe tener y que tan avanzado deben ser? (Ej. Swift UI - intermedio)",
+        answers: [" "]
+    },
+    {
+        id: 5,
+        question: "¿Existen circunstancias por las que no se contrataría a un perfil? (Ej. Trabajar en cierta empresa, rotación menor a 1 año en empresas)",
+        answers: [" "]
+    },
+    {
+        id: 6,
+        question: "¿Requiere que el candidato cuente con alguna certificación? De ser positiva su respuesta ¿Cuál sería?",
+        answers: [" "]
+    },
+    {
+        id: 7,
+        question: "¿Le gustaría que nuestro Hunting se enfoque en alguna escuela y carrera en particular?",
+        answers: [" "]
+    },
+    {
+        id: 8,
+        question: "¿Su candidato ideal debe tener un nivel inglés?",
+        answers: ["si", "No"]
+    },
+    {
+        id: 9,
+        question: "En caso de que su respuesta sea afirmativa, ¿Qué nivel de inglés sería ideal para usted?",
+        answers: [ "Básico", "Intermedio", "Avanzado", "Bilingüe", "Nativo"]
+    },
+    {
+        id: 10,
+        question: "¿Este perfil tendrá personal a su cargo?",
+        answers: ["Si", "No"]
+    },
+    {
+        id: 11,
+        question: "Este perfil será tomador de decisiones o cambios fundamentales en la organización, equipo de trabajo o empresa?",
+        answers: ["Si", "No"]
+    },
+    {
+        id: 12,
+        question: "¿Cuál considera que debería ser el punto más fuerte en nuestra búsqueda de su candidato ideal?",
+        answers: [" "]
+    },
+    {
+        id: 13,
+        question: "¿Cuál sería el cuadro de compensaciones que estaría ofreciendo para cubrir su vacante?",
+        answers: [" "]
+    },
+    {
+        id: 14,
+        question: "¿Cuál sería la modalidad de trabajo de su vacante?",
+        answers: ["Remota", "Presencial", "Híbrida (Días en home office y días en oficina)"]
+    },
+    {
+        id: 15,
+        question: "¿Cuál es la ubicación preferid de trabajo?",
+        answers: [" "]
+    },
+    {
+        id: 16,
+        question: "¿Puede brindarnos un horario disponible para presentarle un scouting de su vacante y definir dudas o comentarios respecto a su perfil?",
+        answers: [" "]
     }
 ]
